012fb8db302cf1344fabb3be75d40b9916da4323
Aashish Thota <thotaaashish10@gmail.com>
2020-06-22 08:39:12 -0500

    Initial setup

---
+++ PMtool/src/main/java/com/example/demo/PMtoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PMtoolApplication.class, args);
+	}
+
+}
